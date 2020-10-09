093bf92cbc2c16b43957a53b46e6fbb35aba2adf
Eklavya Chandra <70642430+EklavyaChandra@users.noreply.github.com>
2020-10-09 18:21:06 +0530

    Updated main.py
    Updated to 4 numbers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 a = int(input("Enter the first number"))
 b = int(input("Enter the second number"))
 c = int(input("Enter the third number"))
-d = a+b+c
-print("The sum is", d)
+d = int(input("Enter the fourth number"))
+e = a+b+c
+print(e)
